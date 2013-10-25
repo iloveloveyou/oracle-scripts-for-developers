39da611575e0cb72e4665f1b71d132178813616c
Venkata Bhattaram <tinitiate@users.noreply.github.com>
2013-10-25 15:58:57 -0400

    Create super-debugger.sql

---
+++ super-debugger.sql
@@ -0,0 +1,8 @@
+-- Venkata Bhattaram
+-- tiniiate.com
+
+-- Spec for an awesome debug package
+-- 1) Convert Procedure to Function and Function to Procedure.
+-- 2) Create a Object and Type with 10 Int, 10 Number, 10 Date, 10 Systimestamp, 2 CLOB
+-- 3) Use (2) to create a package to convert RefCursor to Nested Table, so that it can be selected 
+--    using the table function.
